71e1558e7c198a18ebf910647de02e3d81d675e9
Coco <Coco>
2021-02-10 17:11:24 -0800

    Searching

--- src/main/java/us/matthey/coco/algorithm/epi/ch12searching/Searching.java
+++ src/main/java/us/matthey/coco/algorithm/epi/ch12searching/Searching.java
@@ -45,4 +45,96 @@ public class Searching {
         }
         return result;
     }
+
+    public static int searchEntryEqualToItsIndex(List<Integer> A) {
+        int left = 0, right = A.size() - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            int difference = A.get(mid) - mid;
+            if (difference == 0) {
+                return mid;
+            } else if (difference > 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchSmallest(List<Integer> A) {
+        int left = 0, right = A.size() - 1;
+        while (left < right) {
+            int mid = left + ((right - left) / 2);
+            if (A.get(mid) > A.get(right)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static int searchSmallesRepeated(List<Integer> A) {
+        return searchSmallesHelper(A, 0, A.size() - 1);
+    }
+
+    private static int searchSmallesHelper(List<Integer> A, int left, int right) {
+        if (left == right) return left;
+        int mid = left + ((right - left) / 2);
+        if (A.get(mid) > A.get(right)) {
+            return searchSmallesHelper(A,mid + 1, right);
+        } else if (A.get(mid) < A.get(right)) {
+            return searchSmallesHelper(A,left, mid);
+        } else {
+            int leftResult = searchSmallesHelper(A, left, mid);
+            int rightResult = searchSmallesHelper(A, mid+1, right);
+            return A.get(rightResult) < A.get(leftResult) ? rightResult : leftResult;
+        }
+    }
+
+    public static int squareRoot(int k) {
+        long left = 0, right = k;
+        while (left <= right) {
+            long mid = left + ((right - left) / 2);
+            long midSquared= mid * mid;
+            if (midSquared <= k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int)left - 1;
+    }
+
+    public static double squareRootDouble(double x) {
+        double left, right;
+        if (x < 1.0) {
+            left = x;
+            right = 1.0;
+        } else {
+            left = 1.0;
+            right = x;
+        }
+        while (compare(left, right) == Ordering.SMALLER) {
+            double mid = left + 0.5 * (right - left);
+            double midSquared = mid * mid;
+            if (compare(midSquared, x) == Ordering.EQUAL) {
+                return mid;
+            } else if (compare(midSquared, x) == Ordering.LARGER) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
+    }
+
+    private enum Ordering {SMALLER, EQUAL, LARGER}
+
+    private static Ordering compare(double a, double b) {
+        final double EPSILON = 0.00001;
+        double diff = (a- b) / b;
+        return diff < -EPSILON ? Ordering.SMALLER : (diff > EPSILON ? Ordering.LARGER : Ordering.EQUAL);
+    }
 }
