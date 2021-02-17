d7d8c7598e9f997eca15af83c193fcbd8cdafc57
Coco <Coco>
2021-02-17 09:57:51 -0800

    Binary search trees

--- src/main/java/us/matthey/coco/algorithm/epi/ch14sorting.java
+++ src/main/java/us/matthey/coco/algorithm/epi/ch14sorting.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class ch14sorting {
-    public static List<Integer> intersactTwoSortedArraysOneSmallOneBig(List<Integer> A, List<Integer> B) {
+    public static List<Integer> intersactTwoSortedArraysBinarySearch(List<Integer> A, List<Integer> B) {
         /* Best solution if one set is much smaller then the other */
         List<Integer> intersactionAB = new ArrayList<>();
         for (int i = 0; i < A.size(); i++) {
@@ -20,7 +20,7 @@ public class ch14sorting {
         List<Integer> intersactionAB = new ArrayList<>();
         int i = 0, j = 0;
         while (i < A.size() && j < B.size()) {
-            if (A.get(i) == B.get(j) && (i==0 || A.get(i) != A.get(i-1))) {
+            if (A.get(i) == B.get(j) && (i == 0 || A.get(i) != A.get(i - 1))) {
                 intersactionAB.add(A.get(i));
                 i++;
                 j++;
@@ -31,5 +31,107 @@ public class ch14sorting {
             }
         }
         return intersactionAB;
+    }
+
+    public static void mergeTwoSortedArrays(List<Integer> A, int m, List<Integer> B, int n) {
+        int a = m - 1, b = n - 1, writeIdx = m + n - 1;
+        while (a >= 0 && b >= 0) {
+            A.set(writeIdx--, A.get(a) > B.get(b) ? A.get(a--) : B.get(b--));
+        }
+        while (b >= 0) {
+            A.set(writeIdx--, B.get(b--));
+        }
+    }
+
+    public static class Name implements Comparable<Name> {
+        String firstName;
+        String lastName;
+
+        @Override
+        public int compareTo(Name name) {
+            int cmpFist = firstName.compareTo(name.firstName);
+            if (cmpFist != 0) return cmpFist;
+            return lastName.compareTo(name.lastName);
+        }
+    }
+
+    public static void EliminateDuplicate(List<Name> A) {
+        Collections.sort(A);
+        int result = 0;
+        for (int first = 1; first < A.size(); first++) {
+            if (!A.get(first).firstName.equals(A.get(result).firstName)) {
+                A.set(++result, A.get(first));
+            }
+        }
+        A.subList(++result, A.size()).clear();
+    }
+
+    public static class Event {
+        public int start, end;
+
+        public Event(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    private static class Endpoint implements Comparable<Endpoint> {
+        public int time;
+        public boolean isStart;
+
+        @Override
+        public int compareTo(Endpoint e) {
+            if (time != e.time) {
+                return Integer.compare(time, e.time);
+            }
+            return isStart && !e.isStart ? -1 : !isStart && e.isStart ? 1 : 0;
+        }
+
+        Endpoint(int t, boolean is) {
+            time  = t;
+            isStart = is;
+        }
+    }
+
+    public static int findMaxSimutaneousEvents(List<Event> A) {
+        List<Endpoint> E = new ArrayList<>();
+        for (Event event : A) {
+            E.add(new Endpoint(event.start, true));
+            E.add(new Endpoint(event.end, false));
+        }
+        Collections.sort(E);
+        int maxNumSimutaneousEvents = 0, numSimutaneousEvents = 0;
+        for (Endpoint e : E) {
+            if (e.isStart) {
+                ++numSimutaneousEvents;
+                maxNumSimutaneousEvents = Math.max(maxNumSimutaneousEvents, numSimutaneousEvents);
+            } else {
+                --numSimutaneousEvents;
+            }
+        }
+        return maxNumSimutaneousEvents;
+    }
+
+    public static class Interval {
+        public int left, right;
+        public Interval(int l, int r) {
+            this.left = l;
+            this.right = r;
+        }
+    }
+
+    public static List<Interval> addInterval(List<Interval> disjointIntervals, Interval newInterval) {
+        int i = 0;
+        List<Interval> results = new ArrayList<>();
+        while (i < disjointIntervals.size() && disjointIntervals.get(i).right < newInterval.left) {
+            results.add(disjointIntervals.get(i++));
+        }
+        while (i < disjointIntervals.size()) {
+            newInterval = new Interval(Math.min(newInterval.left, disjointIntervals.get(i).left), Math.max(newInterval.right, disjointIntervals.get(i).right));
+            ++i;
+        }
+        results.add(newInterval);
+        results.addAll(disjointIntervals.subList(i, disjointIntervals.size()));
+        return results;
     }
 }
--- src/main/java/us/matthey/coco/algorithm/epi/ch15binarysearchtrees/BinarySearchTrees.java
+++ src/main/java/us/matthey/coco/algorithm/epi/ch15binarysearchtrees/BinarySearchTrees.java
@@ -1,12 +1,20 @@
 package us.matthey.coco.algorithm.epi.ch15binarysearchtrees;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class BinarySearchTrees {
     public static class BSTNode<T> {
         public T data;
         public BSTNode<T> left, right;
+
+        public BSTNode(T data, BSTNode<T> left, BSTNode<T> right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
     }
 
     public static BSTNode<Integer> searchBST(BSTNode<Integer> tree, int key) {
@@ -55,4 +63,75 @@ public class BinarySearchTrees {
         }
         return true;
     }
+
+    public static BSTNode<Integer> findFirstGreaterThanK(BSTNode<Integer> tree, Integer k) {
+        BSTNode<Integer> subtree = tree, firstSoFar = null;
+        while (subtree != null) {
+            if (subtree.data > k) {
+                firstSoFar = subtree;
+                subtree = subtree.left;
+            } else {
+                subtree = subtree.right;
+            }
+        }
+        return subtree;
+    }
+
+    public static List<Integer> findKLargestInBST(BSTNode<Integer> tree, int k) {
+        List<Integer> KLargestElements = new ArrayList<>();
+        findKLargestInBSTHelper(tree, k, KLargestElements);
+        return KLargestElements;
+    }
+
+    private static void findKLargestInBSTHelper(BSTNode<Integer> tree, int k, List<Integer> KLargestElements) {
+        if (tree != null && KLargestElements.size() < k) {
+            findKLargestInBSTHelper(tree.right, k, KLargestElements);
+            if (KLargestElements.size() < k) {
+                KLargestElements.add(tree.data);
+                findKLargestInBSTHelper(tree.left, k, KLargestElements);
+            }
+        }
+    }
+
+    public static BSTNode<Integer> findLCA(BSTNode<Integer> tree, BSTNode<Integer> s, BSTNode<Integer> b) {
+        BSTNode<Integer> p = tree;
+        while (p.data < s.data || p.data > b.data) {
+            while (p.data < s.data) p = p.right;
+            while (p.data > b.data) p = p.left;
+        }
+        return p;
+    }
+
+    public static BSTNode<Integer> rebuildBSTFromPreorderN2(List<Integer> preorderSequence) {
+        return rebuildBSTFromPreorderHelper(preorderSequence, 0, preorderSequence.size());
+    }
+
+    private static BSTNode<Integer> rebuildBSTFromPreorderHelper(List<Integer> preorderSequence, int start, int end) {
+        if (start >= end) return null;
+        int transit = start + 1;
+        while (transit < end && Integer.compare(preorderSequence.get(transit), preorderSequence.get(start)) < 0) {
+            ++transit;
+        }
+        return new BSTNode<>(preorderSequence.get(start),
+                rebuildBSTFromPreorderHelper(preorderSequence, start + 1, transit),
+                rebuildBSTFromPreorderHelper(preorderSequence, transit, end));
+    }
+    
+    private static Integer rootIdx;
+
+    public static BSTNode<Integer> rebuildBSTFromPreorderN(List<Integer> preorderSequence) {
+        rootIdx = 0;
+        return rebuildBSTFromPreorderOnValueRange(preorderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static BSTNode<Integer> rebuildBSTFromPreorderOnValueRange(List<Integer> preorderSequence, int lowerBound, int upperBound) {
+        if (rootIdx == preorderSequence.size()) return null;
+        Integer root = preorderSequence.get(rootIdx);
+        if (rootIdx < lowerBound || rootIdx > upperBound) return null;
+        ++rootIdx;
+        BSTNode<Integer> leftSubtree = rebuildBSTFromPreorderOnValueRange(preorderSequence, lowerBound, root);
+        BSTNode<Integer> rightSubtree = rebuildBSTFromPreorderOnValueRange(preorderSequence, root, upperBound);
+        return new BSTNode<>(root, leftSubtree, rightSubtree);
+    }
+
 }
